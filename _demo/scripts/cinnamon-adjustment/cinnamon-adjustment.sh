2be3c939000a44b5760ce63483e80039dace3d36
samwhelp <samwhelp@users.noreply.github.com>
2025-01-24 12:21:33 +0000

    fix

--- _demo/scripts/cinnamon-adjustment/cinnamon-adjustment.sh
+++ _demo/scripts/cinnamon-adjustment/cinnamon-adjustment.sh
@@ -22,9 +22,22 @@
 
 mod_cinnamon_config_for_mouse_button_modifier () {
 
+
+	##
+	## ## Disable Super_L Open Menu
+	##
+
+	## > None
+
+
+	##
+	## ## Mouse Button Modifier
+	##
+
 	gsettings set org.cinnamon.desktop.wm.preferences mouse-button-modifier "'<Super>'"
 
 	gsettings set org.cinnamon.desktop.wm.preferences resize-with-right-button true
+
 
 	return 0
 }
